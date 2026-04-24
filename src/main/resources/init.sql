8165134401f503b41c7f4a761c42aef9b59b2756
Florian Goerke <florian@goerke.de>
2026-04-24 08:40:09 +0200

    feat: sample data

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS Resources;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Groups;
+DROP TABLE IF EXISTS Permissions;
+DROP TABLE IF EXISTS UserGroups;
+DROP TABLE IF EXISTS UserResourcePermissions;
+DROP TABLE IF EXISTS GroupResourcePermissions;
+DROP TABLE IF EXISTS AuthMethods;
+
 -- create core tables
 CREATE TABLE Resources (
     resourceId INT PRIMARY KEY,
@@ -59,22 +68,6 @@ CREATE TABLE Logs (
     FOREIGN KEY (resourceId) REFERENCES Resources(resourceId)
 );
 
-
--- TODO query "all permissions for a user" using UNION on both UserResourcePermissions and GroupResourcePermissions
-SELECT P.name FROM UserResourcePermissions UP JOIN Permissions P ON UP.permId = P.permId WHERE UP.userId = 0;
-
---....
-UNION
-SELECT P.name FROM GroupResourcePermissions GP JOIN Permissions P ON GP.permId = P.permId WHERE GP.groupId = 0;
-
--- TODO view for ^ (Effective permissions per user)
--- TODO view for "user access summary" (which users can access which resources?) use case: dashboard
--- TODO view for permissions from a users view (can only see own perms)
--- TODO trigger for checking new grants (ex. user cant have direct AND group permission)
-
--- TODO sample data
-
-
 -- WIP "Authentication"
 CREATE TABLE AuthMethods (
     AuthMethodId INT PRIMARY KEY,
@@ -82,11 +75,62 @@ CREATE TABLE AuthMethods (
     type VARCHAR, -- "password", "api_token"
     super_secret_hash VARCHAR,
     creation DATE,
-    expires DATE
+    expires DATE,
 );
 
--- TODO java API for authentication
+-- sample data 
+INSERT INTO Users (userId, username, vorname, nachname, lastLogin) VALUES
+(0, 'gurki', 'Florian', 'Goerke', '2026-04-24'),
+(1, 'amax', 'Anna', 'Max', '2026-04-01'),
+(2, 'mweber', 'Max', 'Weber', '2026-04-02'),
+(3, 'lschmidt', 'Laura', 'Schmidt', '2026-04-03'),
+(4, 'tmueller', 'Tom', 'Müller', '2026-04-04'),
+(5, 'smeier', 'Sophie', 'Meier', '2026-04-05'),
+(6, 'jbecker', 'Jonas', 'Becker', '2026-04-06'),
+(7, 'ehoffmann', 'Emma', 'Hoffmann', '2026-04-07'),
+(8, 'lfischer', 'Leon', 'Fischer', '2026-04-08'),
+(9, 'ngrant', 'Nina', 'Grant', '2026-04-09'),
+(10, 'pbauer', 'Paul', 'Bauer', '2026-04-10'),
+(11, 'hmeyer', 'Hannah', 'Meyer', '2026-04-11'),
+(12, 'fgross', 'Felix', 'Groß', '2026-04-12'),
+(13, 'klehmann', 'Klara', 'Lehmann', '2026-04-13'),
+(14, 'dwagner', 'David', 'Wagner', '2026-04-14'),
+(15, 'lkoch', 'Lena', 'Koch', '2026-04-15'),
+(16, 'nrichter', 'Noah', 'Richter', '2026-04-16'),
+(17, 'mmartin', 'Mia', 'Martin', '2026-04-17'),
+(18, 'fschneider', 'Finn', 'Schneider', '2026-04-18'),
+(19, 'jwolf', 'Julia', 'Wolf', '2026-04-19'),
+(20, 'lgraf', 'Lukas', 'Graf', '2026-04-20'),
+(21, 'avalentin', 'Amelie', 'Valentin', '2026-04-21'),
+(22, 'obenner', 'Oskar', 'Benner', '2026-04-22'),
+(23, 'mkrueger', 'Marie', 'Krüger', '2026-04-23'),
+(24, 'eroth', 'Elias', 'Roth', '2026-04-24'),
+(25, 'cklein', 'Clara', 'Klein', '2026-03-25'),
+(26, 'nengel', 'Nico', 'Engel', '2026-03-26'),
+(27, 'lsommer', 'Lea', 'Sommer', '2026-03-27'),
+(28, 'ahuber', 'Anton', 'Huber', '2026-03-28'),
+(29, 'mvoigt', 'Mila', 'Voigt', '2026-03-29'),
+(30, 'jbrandt', 'Jan', 'Brandt', '2026-03-30');
 
+-- example permission, accessing a door?
+INSERT INTO Permissions VALUES (0, 'doorAccess');
+
+-- example resource, here its our class door
+INSERT INTO Resources VALUES (0, 'door_p143', '');
+
+-- give user 0 access to door_p143
+INSERT INTO UserResourcePermissions VALUES (0, 0, 0);
+
+-- get direct permissions of user 0 
+SELECT P.name FROM UserResourcePermissions UP JOIN Permissions P ON UP.permId = P.permId WHERE UP.userId = 0;
+
+-- get permissions of group 0
+SELECT P.name FROM GroupResourcePermissions GP JOIN Permissions P ON GP.permId = P.permId WHERE GP.groupId = 0;
+
+-- TODO view for ^ (Effective permissions per user)
+-- TODO view for "user access summary" (which users can access which resources?) use case: dashboard
+-- TODO view for permissions from a users view (can only see own perms)
+-- TODO trigger for checking new grants (ex. user cant have direct AND group permission)
 
 --Trigger--
 CREATE TRIGGER trg_user_login
